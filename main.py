da59d3ae6b75a227cbfce35c598fdc62f5d1e161
Ali Sheikh <l_lawliet786@hotmail.com>
2026-07-24 23:35:04 -0400

    broadcasting to multiple clients

--- main.py
+++ main.py
@@ -16,12 +16,31 @@ def get_chat_page():
     return FileResponse("static/index.html")
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await websocket.send_text(f"Echo: {data}")
+            await manager.broadcast(data)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        manager.disconnect(websocket)
